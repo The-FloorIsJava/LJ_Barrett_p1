bdbb55d750db686674671bba6441be9db1ae229c
LJB0695 <barrettny4@gmail.com>
2022-11-22 09:51:14 -0500

    added to the employee controller class

--- src/main/java/Controller/EmployeeController.java
+++ src/main/java/Controller/EmployeeController.java
@@ -1,4 +1,20 @@
 package Controller;
 
+import Service.EmployeeService;
+import io.javalin.Javalin;
+
 public class EmployeeController {
+    EmployeeService employeeService;
+    Javalin app;
+
+    public EmployeeController(Javalin app) {
+        employeeService = new EmployeeService(new EmployeeDAO());
+        this.app = app;
+    }
+
+    public void employeeEndpoint() {
+        app.get("hello", this::helloHandler);
+    }
+
+
 }
